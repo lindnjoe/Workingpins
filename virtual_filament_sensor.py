4dd7e6fd2cd6465e607b4587bd1d964b1179aa7a
lindnjoe <32586000+lindnjoe@users.noreply.github.com>
2025-07-25 21:12:18 -0400

    Export prefix handlers

--- virtual_filament_sensor.py
+++ virtual_filament_sensor.py
@@ -7,9 +7,12 @@ from .filament_switch_sensor import VirtualSwitchSensor
 
 def load_config_prefix(config):
     """Config handler for [virtual_filament_sensor] sections."""
-    pin = config.get('pin')
-    if pin.startswith('ams_pin:'):
-        vpin_name = pin.split('ams_pin:', 1)[1].strip()
+    pin = config.get("pin")
+    if pin.startswith("ams_pin:"):
+        vpin_name = pin.split("ams_pin:", 1)[1].strip()
     else:
         vpin_name = pin.strip()
     return VirtualSwitchSensor(config, vpin_name)
+
+
+__all__ = ["load_config_prefix"]
